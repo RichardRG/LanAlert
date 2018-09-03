044a0d6e48eb8d59fc630f5c7d7903c3ddf46621
Richard Cook <rcook@rategenius.com>
2018-09-03 13:18:30 -0500

    Feature updates and notation
    Added agent auto population, SlackID must now be set inside AD or manually populated to the Info tab for non AD users.
    
    Notes added to the main body of code.

--- LanAlert/LanAlertMain.py
+++ LanAlert/LanAlertMain.py
@@ -5,7 +5,6 @@ from time import sleep
 from datetime import datetime
 import json
 
-config = {}
 with open('config.cfg','r') as configimport:
     config = json.load(configimport)
 
@@ -23,37 +22,55 @@ slackusername = config['slackbotusername']
 
 url = config['webaddress']
 
-startctcnquery = 'SELECT ' \
-        '( SELECT TOP 1 MAX(htblticket.ticketid) as "Col 1" ' \
-        'FROM [lansweeperdb].[dbo].[htblticket]) AS col1, ' \
-        '(SELECT TOP 1 MAX(htblnotes.noteid) as "Col 2" ' \
-        'FROM [lansweeperdb].[dbo].[htblnotes]) as col2'
+startctcnquery = '''SELECT  
+        ( SELECT TOP 1 MAX(htblticket.ticketid) as "Col 1"  
+        FROM [lansweeperdb].[dbo].[htblticket]) AS col1,  
+        (SELECT TOP 1 MAX(htblnotes.noteid) as "Col 2"  
+        FROM [lansweeperdb].[dbo].[htblnotes]) as col2'''
 
-ticketquery = 'SELECT TOP %s ' \
-          'htblticket.ticketid,' \
-          'htblticket.subject,' \
-          'htblusers.name,' \
-          'htblnotes.note, ' \
-          'htblnotes.noteid ' \
-          'FROM [lansweeperdb].[dbo].[htblticket] ' \
-          'inner join lansweeperdb.dbo.htblnotes On htblticket.ticketid = htblnotes.ticketid ' \
-          'inner Join lansweeperdb.dbo.htblusers On htblticket.fromuserid = htblusers.userid ' \
-          'WHERE notetype=\'3\' ORDER BY ticketid DESC'
+ticketquery = '''SELECT TOP %s  
+          htblticket.ticketid, 
+          htblticket.subject, 
+          htblusers.name, 
+          htblnotes.note,  
+          htblnotes.noteid  
+          FROM [lansweeperdb].[dbo].[htblticket]  
+          inner join lansweeperdb.dbo.htblnotes On htblticket.ticketid = htblnotes.ticketid  
+          inner Join lansweeperdb.dbo.htblusers On htblticket.fromuserid = htblusers.userid  
+          WHERE notetype=3 ORDER BY ticketid DESC'''
 
-notequery = 'SELECT TOP %s ' \
-          'htblticket.ticketid,' \
-          'htblticket.subject, ' \
-          'htblusers.name,' \
-          'htblnotes.note, ' \
-          'htblticket.agentid, ' \
-          'htblnotes.userid, ' \
-          'htblnotes.noteid, ' \
-          'htblnotes.notetype ' \
-          'FROM [lansweeperdb].[dbo].[htblticket] ' \
-          'inner join lansweeperdb.dbo.htblnotes On htblticket.ticketid = htblnotes.ticketid ' \
-          'inner Join lansweeperdb.dbo.htblusers On htblticket.fromuserid = htblusers.userid ' \
-          'ORDER BY noteid DESC'
+notequery = '''SELECT TOP %s  
+          htblticket.ticketid, 
+          htblticket.subject,  
+          htblusers.name, 
+          htblnotes.note,  
+          htblticket.agentid,  
+          htblnotes.userid,  
+          htblnotes.noteid,  
+          htblnotes.notetype  
+          FROM [lansweeperdb].[dbo].[htblticket]  
+          inner join lansweeperdb.dbo.htblnotes On htblticket.ticketid = htblnotes.ticketid  
+          inner Join lansweeperdb.dbo.htblusers On htblticket.fromuserid = htblusers.userid  
+          ORDER BY noteid DESC'''
 
+agentquery = '''SELECT  
+            htblagents.agentid, 
+            htblagents.userid, 
+            tblADusers.Info  
+            FROM [lansweeperdb].[dbo].[htblagents]  
+            LEFT JOIN htblusers on htblusers.userid = htblagents.userid  
+            LEFT JOIN tblADusers on htblusers.username = tblADusers.Username  
+            WHERE active = 1'''
+slackid = {}
+agenttouser = {}
+#Sets agents slack id from agent config and user config in lansweeper, slack ID comes from
+try:
+    for agent in sqlquery(agentquery,username,password,server):
+        slackid[str(agent[0])] = agent[2]
+        agenttouser[str(agent[0])] = str(agent[1])
+except Exception as e:
+    logfile.write('Config - Error setting slack and agent table ' + str(e) + '\n')
+print slackid
 currentticket,currentnote = sqlquery(startctcnquery, username, password, server)[0]
 try:
     while True:
@@ -62,17 +79,23 @@ try:
         try:
             outputticket = []
             newticket = sqlquery(startctcnquery, username, password, server)[0][0]
+            #Check for new ticket
             if newticket > currentticket:
+                #Queries all new tickets
                 tickets = sqlquery(ticketquery % str(int(newticket) - int(currentticket)), username, password, server)
                 currentticket = newticket
                 for ticket in tickets:
+                    # Strips HTML and signature from emails and returns clean body
                     body = soup(ticket[3])
+                    # Formats clickable link for slack
                     ticketlink = '<'+ url + '%s | Ticket %s %s>' % (ticket[0],ticket[0],ticket[1])
+                    # formats subject
                     subject = '*%s* \n *From: %s --* \n' % (ticketlink,ticket[2])
                     outputticket.append(subject + body)
             else:
                 print date + ' - ' + time + ' - Pass Ticket'
             for messages in outputticket:
+                #Sends all new tickets to the main channel
                 response = posttoslack(messages, slacktoken, ticketchannel, slackusername, server)
                 if str(response['ok']).lower() != 'true':
                     logfile.write(date + ' - ' + time + ' - Tickets - ' + str(response) + '\n')
@@ -80,22 +103,34 @@ try:
             logfile.write(date + ' - ' + time + ' - Tickets Error - ' + str(e) + '\n')
         try:
             outputnote = []
+            #Queries the latest note
             newnote = sqlquery(startctcnquery, username, password, server)[0][1]
+            #Checks if there is a new note
             if newnote > currentnote:
                 notes = sqlquery(notequery % str(int(newnote) - int(currentnote)), username, password, server)
                 currentnote = newnote
                 for note in notes:
-                    if str(note[5]) not in config['users']['userid'].values():
+                    # Adds all notes that are not from agents to the outputnote var
+                    if str(note[5]) not in agenttouser.values():
+                        #Strips HTML and signature from emails and returns clean body
                         body = soup(note[3])
+                        #Formats clickable link for slack
                         notelink = '<'+ url + '%s | Update on Ticket %s %s>' % (note[0], note[0], note[1])
+                        #formats subject
                         subject = '*%s* \n *From: %s --* \n' % (notelink, note[2])
                         outputnote.append([subject+body,note[4]])
 
             else:
                 print date + ' - ' + time + ' - Pass Note'
+            #Digests outputnote
             if outputnote:
                 for message in outputnote:
-                    channelid = openslackchannel(config['users']['slackid'][str(message[1])],slacktoken)
+                    #Determines if an agent has been assigned, if not updates go to the main channel
+                    if message[1]:
+                        channelid = openslackchannel(slackid[str(message[1])],slacktoken)
+                    else:
+                        channelid = {"ok":"true", "channel":{"id":ticketchannel}}
+                    #Sends formatted note to the agent or main channel
                     if str(channelid['ok']).lower() == 'true':
                         response = posttoslack(message[0],slacktoken,channelid['channel']['id'],slackusername)
                         if str(response['ok']).lower() != 'true':
